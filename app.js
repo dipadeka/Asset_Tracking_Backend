d77ec7856f716a722d6c5dc35714608657c4462f
dipadeka <dipadeka13@gmail.com>
2026-02-15 19:33:54 +0530

    home-page

---
+++ app.js
@@ -0,0 +1,25 @@
+const http=require('http')
+const {readFile}=require('fs');
+
+
+const server=http.createServer((req, res)=>{
+  const url=req.url
+  if(url==='/'){
+    res.writeHead(200, {'Content-Type': 'text/html'})
+    res.write('<h1>home page</h1>')
+    res.end()
+  } else if(url==='/about'){
+    res.writeHead(200, {'Content-Type': 'text/html'})
+    res.write('<h1>about page</h1>')
+    res.end()
+  } else {
+    res.writeHead(404, {'Content-Type': 'text/html'})
+    res.write('<h1>page not found</h1>')
+    res.end()
+  }
+})
+
+server.listen(5000, () => {
+   console.log('Server is running on http://localhost:5000')
+})
+   
