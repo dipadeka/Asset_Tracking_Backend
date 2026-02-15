445c50a1a7a3f464e83fa848511d7421695f67d5
dipadeka <dipadeka13@gmail.com>
2026-02-15 20:17:23 +0530

    server

--- app.js
+++ app.js
@@ -1,26 +1,6 @@
-const http=require('http')
-const {readFile}=require('fs');
+const express=require('express');
+const app=express()
 
-const homePage=readFile('./index.html')
-
-const server=http.createServer((req, res)=>{
-  const url=req.url
-  if(url==='/'){
-    res.writeHead(200, {'Content-Type': 'text/html'})
-    res.write(homePage)
-    res.end()
-  } else if(url==='/about'){
-    res.writeHead(200, {'Content-Type': 'text/html'})
-    res.write('<h1>about page</h1>')
-    res.end()
-  } else {
-    res.writeHead(404, {'Content-Type': 'text/html'})
-    res.write('<h1>page not found</h1>')
-    res.end()
-  }
-})
-
-server.listen(5000, () => {
-   console.log('Server is running on http://localhost:5000')
-})
-   
+app.listen(5000,()=>{
+    console.log('Server is running on http://localhost:5000')
+}
