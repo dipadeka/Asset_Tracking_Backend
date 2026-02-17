e96b4e709548fc213a45967c9f83344003bf859b
dipadeka <dipadeka13@gmail.com>
2026-02-17 19:51:11 +0530

    connect db

--- app.js
+++ app.js
@@ -1,10 +1,47 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const Product = require('./models/product.model');
 const app = express();
+
+app.use(express.json()); // Middleware to parse JSON bodies
 
 // ROUTE FIRST
 app.get('/', (req, res) => {
     res.send("Backend is working ✅");
+});
+
+app.get('/api/products', async (req, res) => {
+    try {
+        const product = await Product.find();
+        res.status(200).json(product);
+    }
+    catch (error) {
+        res.status(500).json({ error: "Internal Server Error" });
+
+    }
+});
+
+app.get('/api/product/:id', async (req, res)) => {
+    try {
+        const {id} = req.params;
+        const product = await Product.findById(id);
+        res.status(200).json(product);
+        }
+    }
+    catch (error) {
+        res.status(500).json({ message:error.message});
+    }
+
+       
+app.post('/api/product',async (req,res)=>{
+    try {
+        const product = await Product.create(req.body);
+        res.status(200).json(product);
+    } catch (error) {
+        console.error("Error processing request:", error);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+
 });
 
 // LISTEN LAST
@@ -13,7 +50,7 @@ app.listen(5000, () => {
 });
 
 // MongoDB connection
-mongoose.connect('mongodb+srv://dipadeka133:Dipajorhat@backenddb.i39qfwt.mongodb.net/?Node-API?retryWrites=true&w=majority&appName=BackendDB')
+mongoose.connect('mongodb+srv://dipadeka133:Dipajorhat@backenddb.i39qfwt.mongodb.net/Node-API?retryWrites=true&w=majority&appName=BackendDB')
   .then(() => {console.log('Connected to MongoDB!');
 })
 .catch(() => {
